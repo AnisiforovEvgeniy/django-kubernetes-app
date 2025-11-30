1ed4ba793fa3772f8fbd09b67b7756e78a7b9ceb
EvgeniyAnisiforov <Anisiforov_04@mail.ru>
2025-11-30 23:41:45 +0300

    Проверка авто-деплоя

--- django_app/app/views.py
+++ django_app/app/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     hostname = socket.gethostname()
-    version_text = "Авто-деплой работает! Версия: 1"
+    version_text = "Авто-деплой работает! Версия: 2"
     return HttpResponse(f"<h2>{version_text}</h2><p>Served by pod: {hostname}</p>")
 
